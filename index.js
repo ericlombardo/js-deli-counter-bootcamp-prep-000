992f2e18f6ca695e8eea28b4f55aa3f6339c4f66
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-08-28 19:46:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number ${katzDeliLine} in line.`
+  
+  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
 }
