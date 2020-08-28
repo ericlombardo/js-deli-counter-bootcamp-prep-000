2fe7ebc8c50e68edc357b93edc2ff17e706d1b99
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-08-28 20:28:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ function currentLine(line) {
     numberedList[i] = `${Number([i]) + 1}. ${line[i]}`
   }
   // return general greeting with array which is numbered list
-  return (`The line is currently: ${numberedList}`);
+  return ('The line is currently: ' + numberedList.join(, ));
 }
