92d0f90b2e1150394c63ad258b339d06d8953524
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-08-28 19:21:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// function to welcome guest
-function takeANumber(lineCount, name) {
-  //adds a person to the line
-  lineCount.push(name);
-  // adds person to the end of the line
-  //properly handles multiple people being added
-  return (`Welcome, ${name}. You are number ${katzDeli} in line.`);
+function takeANumber(katzDeliLine, name) {
+  
 }
