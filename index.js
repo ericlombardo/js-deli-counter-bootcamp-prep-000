73732414a610c1f8cb5977d6b125f64749493081
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-08-28 19:52:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name);
+// add name to line and let them know where they are in it
+function takeANumber(line, name) {
+  // adds name to the end of the array
+  line.push(name);
+  // returns greeting with current name and number in line
+  return (`Welcome, ${name}. You are number ${line.length} in line.`);
+}
 
-  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
+//Notifies who is being served
+function nowServing() {
+  if (Array.isArray(line) )
 }
